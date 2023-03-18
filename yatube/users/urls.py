6843334d32b33e489265fc2b84a5312afe0ad1ba
Алексей Мокляк <LeshaMDev@gmail.com>
2023-03-18 16:06:03 +0300

    Привел в соответствии PEP8

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView, LoginView, PasswordResetView
+from django.contrib.auth.views import LogoutView, LoginView
 from . import views
 
 
